4bf31c7bd13ed97ab81a7b2a9d4c8e35bcffe450
WilliamPerezBeltran <williampbeltranprogramador@gmail.com>
2020-05-14 11:26:52 -0500

    add if statement

--- 3_if_statment.py
+++ 3_if_statment.py
@@ -1,37 +1,19 @@
 import sys 
 
 def main():
-	print strings()
+	print if_statement(speed, mood)
 
-def strings():
-	s = 'hi'
-	print s[0]
-	print s[1]
-	print len(s)
-	print s + ' ole'
-	raw = r'this\t\n and that'
-	print 'LOWER'.lower()
-	a = '---'.join(['aaa', 'bbb', 'ccc'])
-	string = ' william fernando perez '
-	print string.strip()[0]
-	print string.strip()[1]
-	print string.strip()[2]
-	print string.strip()[0:7]
-	print string.strip()[3:7]
-	  # % operator
-	print "%d little %s, and %s, or %s" % (3, 'huff', 'puff', 'house')
-	print "%d the way of concatenate %s " % (100, 'YES')
-	 # Add parentheses to make the long line work:
-	text = ("%d pigs %s, and %s your %s down."
-		% (3, 'huff', 'puff', 'house'))
-	print text
-	# Split the line into chunks, which are concatenated automatically by Python
-	text1 = (
-	"%d little pigs come out, "
-	"or I'll %s, and I'll %s, "
-	"and I'll blow your %s down."
-	% (3, 'huff', 'puff', 'house'))
-	print text1
+def if_statment(speed,mood):
+	if speed >= 80:
+		print 'do something'
+		if mood == 'terrible' or speed > 100:
+			print 'do something'
+		elif mood == 'bad' or speed >= 90:
+			print 'i am going to have to writeyou a ticket'
+			write_ticket()
+		else:
+			print "Let's try to keep it under 80 ok?"
+
 	return ''
 
 if __name__ == '__main__':
