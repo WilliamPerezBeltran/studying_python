42986876cd584b88b2a188b50eae38f7814361c0
WilliamPerezBeltran <williampbeltranprogramador@gmail.com>
2020-05-14 18:14:19 -0500

    Solve string1.py from google-python-exercises

--- google-python-exercises/basic/string1.py
+++ google-python-exercises/basic/string1.py
@@ -23,9 +23,12 @@
 # instead of the actual count.
 # So donuts(5) returns 'Number of donuts: 5'
 # and donuts(23) returns 'Number of donuts: many'
+import pdb
 def donuts(count):
-  # +++your code here+++
-  return
+  result = 'Number of donuts: '
+  if count >= 10:
+    count = 'many'
+  return result + str(count)
 
 
 # B. both_ends
@@ -34,8 +37,9 @@ def donuts(count):
 # so 'spring' yields 'spng'. However, if the string length
 # is less than 2, return instead the empty string.
 def both_ends(s):
-  # +++your code here+++
-  return
+  if len(s) < 2:
+    return ''
+  return s[:2] + s[-2:]
 
 
 # C. fix_start
@@ -48,8 +52,9 @@ def both_ends(s):
 # Hint: s.replace(stra, strb) returns a version of string s
 # where all instances of stra have been replaced by strb.
 def fix_start(s):
-  # +++your code here+++
-  return
+  first_chard =s[0]
+  new_string = s.replace(first_chard,'*')
+  return first_chard + new_string[1:]
 
 
 # D. MixUp
@@ -60,9 +65,11 @@ def fix_start(s):
 #   'dog', 'dinner' -> 'dig donner'
 # Assume a and b are length 2 or more.
 def mix_up(a, b):
-  # +++your code here+++
-  return
 
+  a_swapped = b[:2] +a[:2] 
+  b_swapped = a[:2] + b[:2]
+
+  return "%s %s"%(a_swapped, b_swapped) 
 
 # Provided simple test() function used in main() to print
 # what each function returns vs. what it's supposed to return.
