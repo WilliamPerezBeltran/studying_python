e898a2a1e74dbca5319d3dca484c0b371b8e5cbf
williamperezbeltran <williampbeltranprogramador@gmail.com>
2020-06-03 15:18:49 -0500

    modules in python

---
+++ repaso_python/poo/modulos/saludos.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python -tt
+# coding=utf-8
+
+print "entro a saludos"
+
+def manana():
+	print "Buenos días"
+
+def tarde():
+	print "Buenas tardes"
+
+def noche():
+	print "Buenas noches"
