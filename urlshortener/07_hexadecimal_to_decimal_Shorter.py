1fbd461d91e13ca1547d0ffbdb2487b081144599
WilliamPerezBeltran <williampbeltranprogramador@gmail.com>
2020-07-01 21:29:37 -0500

    add file string_to_base62

--- urlshortener/07_hexadecimal_to_decimal_Shorter.py
+++ urlshortener/07_hexadecimal_to_decimal_Shorter.py
@@ -2,11 +2,23 @@
 # coding=utf-8
 
 # https://es.wikipedia.org/wiki/Sistema_hexadecimal
+# https://codippa.com/convert-hex-to-decimal-in-python/
+"""
+# hex string
+num_hex = '0F'  
+# convert hex to decimal
+num_dec = int(num_hex, 16)
+print('Value in hex:', num_hex)
+print('Value in decimal:', num_dec)
+
+"""
+
 
 import pdb
 import sys
 
 def hexadecimal_to_decimal(hexadecimal_number='3E0A'):
+	# convert hex to decimal
 	return int(hexadecimal_number,16)
 	
 def main():
---
+++ urlshortener/08_string_to_base62.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python -tt
+# coding=utf-8
+
+import sys
+import hashlib
+
+def string_to_base62(url):
+	#convert string to md5
+	md5 = hashlib.md5()
+	md5.update(url.encode('utf-8'))
+	#convert md5 to exadecimal
+	hexa = md5.hexdigest()
+	#convet hexadecimal to decimal 
+	decimal = int(hexa,16)
+	#convert decimal to base62
+	return convert_decimal_to_base62(decimal)
+
+def convert_decimal_to_base62(number):
+	alphabet = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	
+	arrayShortener = []
+	stringShortener = ''
+	while number > 0:
+		arrayShortener.append(number%62)
+		number = number//62
+
+	for position in reversed(arrayShortener):
+		stringShortener+=alphabet[position]
+
+	return stringShortener
+
+
+
+
+
+
+	
+
+def main():
+	arg = sys.argv[1:]
+	print(string_to_base62(arg[0]))
+
+if __name__=='__main__':
+	main()
