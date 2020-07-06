a555fa6177f11bacb77f12787b6b6f02ff0c8588
WilliamPerezBeltran <williampbeltranprogramador@gmail.com>
2020-07-06 12:25:33 -0500

    add files

--- 1.py
+++ 1.py
@@ -18,3 +18,16 @@ while b<10:
 	print(b)
 	a = b
 	b = a+b
+
+def test_var_args(f_arg, *args):
+    print("first normal arg:", f_arg)
+    for arg in args:
+        print("another arg through *argv:", arg)
+
+test_var_args('yasoob', 'python', 'eggs', 'test')
+
+print(test_var_args)
+
+print("****")
+f = test_var_args
+f(1,2,3,4,5) 
