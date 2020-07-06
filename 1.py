e640a908fe31fd759da8ce28a6483988b5a7d99d
WilliamPerezBeltran <williampbeltranprogramador@gmail.com>
2020-07-06 11:57:16 -0500

    add files

---
+++ 1.py
@@ -0,0 +1,20 @@
+# a = 0
+# b = 1
+# while b<10:
+# 	print(b)
+# 	a=b
+# 	b=a+b
+
+
+
+# a,b = 0,1
+# while b<10:
+# 	print(b)
+# 	a,b = b,a+b
+
+
+a,b = 0,1
+while b<10:
+	print(b)
+	a = b
+	b = a+b
---
+++ ejerciciso_daniel/matrices/01.py
@@ -0,0 +1,14 @@
+#valor suministrado 4
+#resultado: 1 4 4 9 9 9 16 16 16 16 16
+
+import sys
+import pdb
+
+def dibujarMatriz(n):
+
+
+def main():
+	dibujarMatriz()
+
+if __name__=='__main__':
+	main()
